0d7448db8d8c2c1df4e6fddc7140790a2586fd27
Paweł Perłakowski <pperlakowski@sii.pl>
2024-06-19 16:00:59 +0200

    WIP Exercise4

---
+++ src/main/scala/sandbox/exercise4/Exercise4.scala
@@ -0,0 +1,13 @@
+package sandbox.exercise4
+
+import cats.Id
+
+object Exercise4 {
+
+  def pure[A](value: A): Id[A] = value
+
+  def map[A, B](initial: Id[A])(func: A => B): Id[B] = func(initial)
+
+  def flatMap[A, B](initial: Id[A])(func: A => Id[B]): Id[B] = func(initial)
+
+}
